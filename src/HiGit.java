f088c9d04dc286cc4585ff6bd112fde3f63d023d
BOOGIE <3208355342@qq.com>
2023-05-10 19:08:32 +0800

    test

---
+++ src/HiGit.java
@@ -0,0 +1,10 @@
+/**
+ * description:
+ * author: BOOGIE
+ * data: 2023/5/10 18:37
+ */
+public class HiGit {
+    public static void main(String[] args) {
+        System.out.println("hi git");
+    }
+}
